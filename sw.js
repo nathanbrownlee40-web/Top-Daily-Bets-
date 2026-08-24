80f1a792230abf3bacbc684932fcb53dec9179e8
nathanbrownlee40-web <nathanbrownlee40@gmail.com>
2026-08-25 00:08:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='top-daily-bets-v5-market-only';
+const CACHE='top-daily-bets-v7-won-lost';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192x192.png','./icon-512x512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
